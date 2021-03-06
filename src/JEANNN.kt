37d9fb3cb8c679b63875a81a4e5433f479472ba0
Marie-Louise <mariemuteteyimana@gmail.com>
2021-03-06 22:19:12 +0300

    first commit

---
+++ src/JEANNN.kt
@@ -0,0 +1,27 @@
+fun main() {
+   name()
+   modulus()
+   sum()
+   personality()
+
+}
+fun name(){
+    println("hello marie")
+}
+fun modulus(){
+    var num1=20
+    var num2=6
+    var modulus=num1%num2
+    println(modulus)
+}
+fun sum(){
+    var num1=6
+    var num2=4
+    var num3=8
+    var num4=8
+    var sum=num1+num2+num3+num4
+    println(sum)
+}
+fun personality(){
+    println("sociable")
+}
